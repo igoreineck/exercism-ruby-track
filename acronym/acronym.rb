63472ee2c08280c3781bb78d2a4b7cac75cbcb52
Igor Eineck <hauzip@gmail.com>
2020-11-15 19:51:55 -0300

    Finished acronym

---
+++ acronym/acronym.rb
@@ -0,0 +1,31 @@
+=begin
+Write your code for the 'Acronym' exercise in this file. Make the tests in
+`acronym_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/acronym` directory.
+=end
+
+class Acronym
+  class << self
+    def abbreviate(phrase)
+      @phrase = phrase
+
+      response = ""
+
+      split_phrase.each { |word| response += upcase_first_letter(word) }
+
+      response
+    end
+
+    private
+
+    def split_phrase
+      @phrase.split(" ")
+    end
+
+    def upcase_first_letter(word)
+      word[0].upcase
+    end
+  end
+end
